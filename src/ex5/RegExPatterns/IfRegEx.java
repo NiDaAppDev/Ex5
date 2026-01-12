fe2ffa73d27a50cabbaf59bfd31c6c7d638bb064
tomer <tomer.levi@mail.huji.ac.il>
2026-01-12 18:00:58 +0200

    איש עם חץ

--- src/ex5/RegExPatterns/IfRegEx.java
+++ src/ex5/RegExPatterns/IfRegEx.java
@@ -1,4 +1,21 @@
 package ex5.RegExPatterns;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static ex5.RegExPatterns.GeneralRegEx.*;
+
 public class IfRegEx {
+    private static final String IF_DECLARATION_LINE =
+            SPACES_INITIAL //Line can start with spaces
+                    + "if" + SPACE_CHAR + "*" //'if' keyword may have spaces after it
+                    + "\\(" //Opening parenthesis
+                    + "([^)]+)\\)" //Condition
+                    + SPACE_CHAR + "*" //There could be spaces between the condition and parentheses
+                    + "\\{" //The body of the if statement starts with a curly bracket
+                    + SPACES_ENDING; //Line can end with spaces
+    /*
+     * Relevant to the statement above:
+     * Group 1 captures the condition of the if statement.
+     */
 }
