fbc9de6538352e3dc789fbf2c754bfb91cdc4491
nitzan-daloomy <nidaappdev@gmail.com>
2026-01-12 20:48:03 +0200

    peanut butter

--- src/ex5/RegExPatterns/MethodRegexPatterns.java
+++ src/ex5/RegExPatterns/MethodRegexPatterns.java
@@ -2,7 +2,7 @@ package ex5.RegExPatterns;
 
 import java.util.regex.Pattern;
 
-import static ex5.RegExPatterns.GeneralRegEx.SPACE_CHAR;
+import static ex5.RegExPatterns.GeneralRegEx.*;
 import static ex5.RegExPatterns.VariableRegExPatterns.*;
 
 public class MethodRegexPatterns {
@@ -22,8 +22,21 @@ public class MethodRegexPatterns {
     private static final String SINGLE_PARAMETER =
             "(" + VARIABLE_TYPE + ")" + SPACE_CHAR +
                     "+(" + VAR_NAME + ")" + SPACE_CHAR + "*";
+    /*
+    * Relevant to the statement above:
+    * Group 1 captures a single variable's type
+    * Group 2 captures a single variable's name
+    */
+
     private static final String PARAMETERS =
             "(?:" + SINGLE_PARAMETER + "(?:," + SINGLE_PARAMETER + ")*" + ")?";
+
+    private static final String METHOD_CALL =
+            SPACES_INITIAL + "(" + VAR_NAME + ")" + SPACE_CHAR + "*" +
+            "\\((.*)\\)" + ENDING_SEMICOLON;
+    /*
+    * Group 1 captures the input parameters
+    * */
 
     public static final Pattern  METHOD_PATTERN =
             Pattern.compile(METHOD_SIGNATURE);
@@ -31,4 +44,6 @@ public class MethodRegexPatterns {
     public static final Pattern  PARAMETERS_PATTERN =
             Pattern.compile(PARAMETERS);
 
+    public static final Pattern  METHOD_CALL_PATTERN =
+            Pattern.compile(METHOD_CALL);
 }
