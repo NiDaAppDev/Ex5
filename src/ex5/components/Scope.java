c603aec99ef06b27b0f43f1f8f27716366cf784f
tomer <tomer.levi@mail.huji.ac.il>
2026-01-16 01:46:01 +0200

    Added all test files, added some self-validate methods within classes, some are dog-shit and wrong, mb. Changed Scope to (if else) instead of (switch case) because I couldn't run the tests otherwise, added some cases to the scope. Added extractVarNames in condition in IfWhileUtils, but still needs to be fixed. Added the main so we can RUNIT. Tried fixing stuff in VariableUtils and in the regex for variables because it was not passing anything for variable tests. Threw 1 every time. Overall I really hope I didn't destroy the project and if so I will pay someone an alpia(1000) to fix this shit. But we are Passing 301/459 tests tho.

--- src/ex5/components/Scope.java
+++ src/ex5/components/Scope.java
@@ -11,6 +11,7 @@ import ex5.utils.*;
 import javax.sound.sampled.Line;
 
 import static ex5.reg_ex_patterns.IfWhileRegExPatterns.SINGLE_CONDITION_PATTERN;
+import static ex5.reg_ex_patterns.MethodRegExPatterns.RETURN_STATEMENT;
 
 public class Scope {
 
@@ -18,7 +19,6 @@ public class Scope {
     private final String[] codeBlock;
 
     private HashMap<String, Variable> variables;
-    private HashMap<String, Variable> allVisibleVariables;
     private HashMap<String, Method> methods;
     private ArrayList<IfWhile> ifWhileStatements;
 
@@ -28,8 +28,7 @@ public class Scope {
         this.parent = parent;
         this.codeBlock = codeBlock;
         this.variables = variables;
-        allVisibleVariables = getAllVisibleVariables();
-        methods = new HashMap<>();
+        this.methods = new HashMap<>();
         ifWhileStatements = new ArrayList<>();
     }
 
@@ -38,7 +37,6 @@ public class Scope {
         this.parent = parent;
         this.codeBlock = codeBlock;
         this.variables = new  HashMap<>();
-        allVisibleVariables = getAllVisibleVariables();
         methods = new HashMap<>();
         ifWhileStatements = new ArrayList<>();
     }
@@ -47,7 +45,6 @@ public class Scope {
         this.parent = null;
         this.codeBlock = codeBlock;
         this.variables = new  HashMap<>();
-        allVisibleVariables = getAllVisibleVariables();
         methods = new HashMap<>();
         ifWhileStatements = new ArrayList<>();
     }
@@ -66,87 +63,109 @@ public class Scope {
     }
 
     public HashMap<String, Variable> getAllVisibleVariables() {
-        if (parent == null) return variables;
-        HashMap<String, Variable> allVariables = parent.getAllVisibleVariables();
-        allVariables.putAll(variables);
-        return allVariables;
+        HashMap<String, Variable> allVars = new HashMap<>();
+        if (parent != null) {
+            allVars.putAll(parent.getAllVisibleVariables());
+        }
+        allVars.putAll(variables);
+        return allVars;
     }
 
-    private void parseCodeBlock() throws Exception{
+    public void parseCodeBlock() throws Exception {
+
         for (int i = 0; i < codeBlock.length; i++) {
             LineReader.LINE_TYPE currentLineType = LineReader.classifyLine(codeBlock[i]);
-            switch (currentLineType) {
-                case IGNORE -> {
-                }
-                case VAR_DEF -> {
-                    for (NameVariablePair varPair :
-                            VariableUtils.extractDeclaredVariables(LineReader.getCurrentGroups())) {
-                        if (variables.containsKey(varPair.getName())) {
-                            //TODO: throw an "illegal" exception.
-                        }
-                        addVariable(varPair.getName(), varPair.getVariable());
-                    }
-                }
-                case VAR_ASSIGN -> {
-                    VariableAssignmentPair varAssignPair =
-                            VariableUtils.extractVariableAssignment(LineReader.getCurrentGroups()[1]);
-                    if (!allVisibleVariables.containsKey(varAssignPair.getName())) {
-                        //TODO: throw an "illegal" exception.
-                    }
-                    VariableUtils.assignValueToVariable(
-                            allVisibleVariables.get(varAssignPair.getName()),
-                            varAssignPair.getValue());
-                }
-                case IF_WHILE -> ifWhileStatements.add(
-                        new IfWhile(
-                                this,
-                                LineReader.getCurrentGroups()[1],
-                                extractSubScopeCodeBlock(i))
-                );
-                case METHOD_DEF -> {
-                    if (parent != null) {
+
+            if (currentLineType == LineReader.LINE_TYPE.IGNORE) {
+                continue;
+            }
+            else if (currentLineType == LineReader.LINE_TYPE.VAR_DEF) {
+                for (NameVariablePair varPair : VariableUtils.extractDeclaredVariables(LineReader.getCurrentGroups(), this)) {
+                    if (variables.containsKey(varPair.getName())) {
                         throw new Exception();
                     }
-                    String[] body = extractSubScopeCodeBlock(i);
-                    Method newMethod = new Method (LineReader.getCurrentGroups(), body, this);
-                    methods.put(newMethod.getMethodName(), newMethod);
-                    i += body.length - 1;
+                    addVariable(varPair.getName(), varPair.getVariable());
                 }
-                case METHOD_CALL -> {
-                    if(!methods.containsKey(LineReader.getCurrentGroups()[1]) || parent == null) {
-                        throw new Exception();
+            }
+            else if (currentLineType == LineReader.LINE_TYPE.VAR_ASSIGN) {
+                VariableAssignmentPair varAssignPair = VariableUtils.extractVariableAssignment(LineReader.getCurrentGroups()[1]);
+                if (!getAllVisibleVariables().containsKey(varAssignPair.getName())) {
+                    throw new Exception();
+                }
+                Variable var = getAllVisibleVariables().get(varAssignPair.getName());
+
+                if (var == null || var.isFinal()) throw new Exception();
+
+                VariableUtils.validateAndAssign(
+                        var,
+                        varAssignPair.getValue(), this);
+            }
+            else if (currentLineType == LineReader.LINE_TYPE.IF_WHILE) {
+                if (parent == null) throw new Exception();
+
+                String condition = LineReader.getCurrentGroups()[1];
+                String[] body = extractSubScopeCodeBlock(i);
+                ifWhileStatements.add(new IfWhile(this, condition, body));
+                i += body.length + 1;
+            }
+            else if (currentLineType == LineReader.LINE_TYPE.METHOD_DEF) {
+                if (parent != null) throw new Exception();
+                String[] methodGroups = LineReader.getCurrentGroups();
+                String[] body = extractSubScopeCodeBlock(i);
+
+                boolean hasReturn = false;
+                for (String line : body) {
+                    if (line.trim().matches(RETURN_STATEMENT)) {
+                        hasReturn = true;
+                        break;
                     }
-
-                    String methodName = LineReader.getCurrentGroups()[1];
-                    String paramString = LineReader.getCurrentGroups()[2];
-
-                    if (!getMethods().containsKey(methodName)) {
-                        throw new Exception();
-                    }
-
-                    Method method = getMethods().get(methodName);
-
-                    List<Variable> args = MethodUtils.getArgsFromCall(paramString, this);
-                    method.call(args);
                 }
-                case ILLEGAL, default -> {//TODO: throw an "illegal" exception.}
-                }
+                if (!hasReturn) throw new Exception();
+
+                Method newMethod = new Method(methodGroups, body, this);
+                methods.put(newMethod.getMethodName(), newMethod);
+
+                i += body.length + 1;
+            }
+            else if (currentLineType == LineReader.LINE_TYPE.METHOD_CALL) {
+                if (parent == null) throw new Exception();
+
+                String methodName = LineReader.getCurrentGroups()[1];
+                String paramString = LineReader.getCurrentGroups()[2];
+
+                if (!getMethods().containsKey(methodName)) throw new Exception();
+
+                Method method = getMethods().get(methodName);
+                List<Variable> args = MethodUtils.getArgsFromCall(paramString, this);
+                method.call(args);
+            }
+            else if (currentLineType == LineReader.LINE_TYPE.RETURN) {
+            }
+            else {
+                throw new Exception();
+            }
+        }
+
+        if (parent == null) {
+            for (Method m : methods.values()) {
+                m.validate();
             }
         }
     }
 
-    private String[] extractSubScopeCodeBlock(int currentIndex) {
+    private String[] extractSubScopeCodeBlock(int currentIndex) throws Exception{
         String[] remainingCode = SubScopeExtractor.getSubScopeCodeBlock(
                 codeBlock,
                 currentIndex + 1,
                 codeBlock.length);
         int blockClosingBracketIndex = SubScopeExtractor.getBlockEndIndex(remainingCode);
         if (blockClosingBracketIndex == -1) {
-            //TODO: throw an "illegal" exception.
+            throw new Exception();
         }
+        int actualEndIndex = (currentIndex + 1) + blockClosingBracketIndex;
         return SubScopeExtractor.getSubScopeCodeBlock(
                 codeBlock,
                 currentIndex + 1,
-                blockClosingBracketIndex);
+                actualEndIndex);
     }
 }
--- src/ex5/utils/VariableUtils.java
+++ src/ex5/utils/VariableUtils.java
@@ -1,55 +1,118 @@
 package ex5.utils;
 
+import ex5.components.Scope;
 import ex5.components.Variable;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 
 import static ex5.reg_ex_patterns.VariableRegExPatterns.*;
 
 public class VariableUtils {
 
-    public static NameVariablePair[] extractDeclaredVariables(String[] data) {
+    public static NameVariablePair[] extractDeclaredVariables(String[] data, Scope scope) throws Exception {
         boolean isFinal = data[1] != null;
         String type = data[2];
+        String content = data[3];
 
-        Matcher varDeclarationsM = VARIABLE_DEFENITION_CONTENT_PATTERN.matcher(data[3]);
-        NameVariablePair[] vars = new NameVariablePair[varDeclarationsM.groupCount()];
-        for (int i = 1; i <= varDeclarationsM.groupCount(); i++) {
-            String[] varInitData = varDeclarationsM.group(i).trim().split("=");
-            String name = varInitData[0].trim();
-            String value = varInitData[1];
-            vars[i] = new NameVariablePair(name, new Variable(isFinal, type, false));
-            if(value == null) {
-                continue;
+        List<NameVariablePair> varsList = new ArrayList<>();
+        Matcher matcher = FIND_SINGLE_VAR_PATTERN.matcher(content);
+        while (matcher.find()) {
+            String match = matcher.group().trim();
+            String name;
+            String value = null;
+            boolean isInitialized = false;
+
+            if (match.contains("=")) {
+                String[] parts = match.split("=");
+                name = parts[0].trim();
+                if (parts.length > 1) {
+                    value = parts[1].trim();
+                    isInitialized = true;
+                }
+            } else {
+                name = match.trim();
             }
-            value = value.trim();
-            assignValueToVariable(vars[i].getVariable(), value);
+            if (isFinal && !isInitialized){
+                throw new Exception();
+            }
+
+            Variable newVar = new Variable(isFinal, type, isInitialized);
+            if (isInitialized) {
+                if (value == null) throw new Exception();
+                validateAndAssign(newVar, value, scope);
         }
-        return vars;
+            varsList.add(new NameVariablePair(name, newVar));
     }
+        return varsList.toArray(new NameVariablePair[0]);
+}
 
-    public static VariableAssignmentPair extractVariableAssignment(String assignmentStatement) {
-        String[] varInitData = assignmentStatement.trim().split("=");
-        String name = varInitData[0].trim();
-        String value = varInitData[1].trim();
-        return new VariableAssignmentPair(name, value);
-    }
+public static VariableAssignmentPair extractVariableAssignment(String assignmentStatement) {
+    String[] varInitData = assignmentStatement.trim().split("=");
+    String name = varInitData[0].trim();
+    String value = varInitData[1].trim();
+    return new VariableAssignmentPair(name, value);
+}
 
-    public static void assignValueToVariable(Variable variable, String value) {
-        if(!isAssignmentLegal(variable.getType(), value)) {
-            //TODO: throw an "illegal" exception.
+public static void validateAndAssign(Variable variable, String value, Scope scope) throws Exception {
+        if (isAssignmentLegal(variable.getType(), value)) {
+            variable.initialize();
+            return;
         }
-        variable.initialize();
+        if (scope != null && scope.getAllVisibleVariables().containsKey(value)) {
+            Variable srcVar = scope.getAllVisibleVariables().get(value);
+            if (!srcVar.isInitialized()) {
+                throw new Exception();
+            }
+            if (areTypesCompatible(variable.getType(), srcVar.getType())){
+                variable.initialize();
+                return;
+            }
+        }
+        throw new Exception();
+}
+public static void assignValueToVariable(Variable variable, String value) throws Exception {
+    if (!isAssignmentLegal(variable.getType(), value)) {
+       throw new Exception();
+    }
+    variable.initialize();
+}
+
+private static boolean isAssignmentLegal(String type, String value) {
+    Matcher valueTypeM = VALUE_TYPE_PATTERN.matcher(value);
+    if (!valueTypeM.matches()) return false;
+
+    if (type.equals(INT_TYPE)) {
+        return valueTypeM.group(4) != null; // only integers
     }
 
-    private static boolean isAssignmentLegal(String type, String value) {
-        Matcher valueTypeM = VALUE_TYPE_PATTERN.matcher(value);
-        return valueTypeM.matches() &&
-                (!type.equals(INT_TYPE) || valueTypeM.group(1) != null) &&
-                (!type.equals(DOUBLE_TYPE) || valueTypeM.group(2) != null) &&
-                (!type.equals(STRING_TYPE) || valueTypeM.group(3) != null) &&
-                (!type.equals(BOOLEAN_TYPE) || valueTypeM.group(4) != null) &&
-                (!type.equals(CHAR_TYPE) || valueTypeM.group(5) != null);
+    if (type.equals(DOUBLE_TYPE)) {
+        // double accepts double OR integer
+        return (valueTypeM.group(5) != null) || (valueTypeM.group(4) != null);
     }
 
+    if (type.equals(STRING_TYPE)) {
+        return valueTypeM.group(1) != null;
+    }
+
+    if (type.equals(BOOLEAN_TYPE)) {
+        // boolean accepts boolean OR numeric values
+        return (valueTypeM.group(3) != null) || (valueTypeM.group(4) != null) || (valueTypeM.group(5) != null);
+    }
+
+    if (type.equals(CHAR_TYPE)) {
+        return valueTypeM.group(2) != null;
+    }
+
+    return false;
+}
+
+    private static boolean areTypesCompatible(String targetType, String sourceType) {
+        if (targetType.equals(sourceType)) return true;
+        if (targetType.equals(DOUBLE_TYPE) && sourceType.equals(INT_TYPE)) return true;
+        if (targetType.equals(BOOLEAN_TYPE) && (sourceType.equals(INT_TYPE)
+                || sourceType.equals(DOUBLE_TYPE))) return true;
+        return false;
+    }
 }
